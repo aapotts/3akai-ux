28855d2aa5930c358cd75bbbb5f8dde136a038c1
Nate Angell <nate@rsmart.com>
2011-12-22 00:25:20 -0800

    ACAD-509: default all OIPP user-configurable profile areas to share only with authenticated users

--- dev/configuration/config_custom_oipp.js
+++ dev/configuration/config_custom_oipp.js
@@ -1,6 +1,8 @@
 define(["config/config"], function(config) {
 
-    // Custom CSS Files to load in
+    /*
+     * Configure authentication method(s). 
+     */
     config.Authentication = {
             "allowInternalAccountCreation": false,
             "internal": true,
@@ -11,6 +13,339 @@ define(["config/config"], function(config) {
             "hideLoginOn": ["/dev", "/dev/index.html", "/dev/create_new_account.html"]
         };
 
+    /*
+     * Configure default user profile areas and behavior.
+     */
+    config.Profile = {
+        /*
+         * This is a collection of profile configuration functions and settings
+         * The structure of the config object is identical to the storage object
+         * When system/me returns profile data for the logged in user the profile_config and profile_data objects could be merged
+         * "label": the internationalizable message for the entry label in HTML
+         * "required": {Boolean} Whether the entry is compulsory or not
+         * "display": {Boolean} Show the entry in the profile or not
+         * "editable": {Boolean} Whether or not the entry is editable
+         * For a date entry field use "date" as the type for MM/dd/yyyy and "dateITA" as the type for dd/MM/yyyy
+         *
+         */
+        configuration: {
+
+            defaultConfig: {
+
+                "basic": {
+                    "label": "__MSG__PROFILE_BASIC_LABEL__",
+                    "required": true,
+                    "display": true,
+                    "access": "everybody",
+                    "modifyacl": false,
+                    "permission": "anonymous",
+                    "order": 0,
+                    "elements": {
+                        "firstName": {
+                            "label": "__MSG__PROFILE_BASIC_FIRSTNAME_LABEL__",
+                            "errorMessage": "__MSG__PROFILE_BASIC_FIRSTNAME_ERROR__",
+                            "required": true,
+                            "display": true,
+                            "limitDisplayLength": 50
+                        },
+                        "lastName": {
+                            "label": "__MSG__PROFILE_BASIC_LASTNAME_LABEL__",
+                            "errorMessage": "__MSG__PROFILE_BASIC_LASTNAME_ERROR__",
+                            "required": true,
+                            "display": true,
+                            "limitDisplayLength": 50
+                        },
+                        "picture": {
+                            "label": "__MSG__PROFILE_BASIC_PICTURE_LABEL__",
+                            "required": false,
+                            "display": false
+                        },
+                        "preferredName": {
+                            "label": "__MSG__PROFILE_BASIC_PREFERREDNAME_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "email": {
+                            "label": "__MSG__PROFILE_BASIC_EMAIL_LABEL__",
+                            "errorMessage": "__MSG__PROFILE_BASIC_EMAIL_ERROR__",
+                            "required": true,
+                            "display": false,
+                            "type": "email"
+                        },
+                        "status": {
+                            "label": "__MSG__PROFILE_BASIC_STATUS_LABEL__",
+                            "required": false,
+                            "display": false
+                        },
+                        "tags": {
+                            "label": "__MSG__TAGS__",
+                            "required": false,
+                            "display": true,
+                            "type": "textarea",
+                            "tagField": true
+                        }
+                    }
+                },
+                "locations": {
+                    "label": "__MSG__PROFILE_LOCATIONS_LABEL__",
+                    "required": false,
+                    "display": true,
+                    "access": "everybody",
+                    "modifyacl": true,
+                    "permission": "anonymous",
+                    "multiple": true,
+                    "directory": true,
+                    "multipleLabel": "__MSG__PROFILE_LOCATION_LABEL__",
+                    "order": 1,
+                    "elements": {
+                        "locationtitle": {
+                            "label": "__MSG__PROFILE_LOCATION_LABEL__",
+                            "required": true,
+                            "display": true,
+                            "type": "location"
+                        }
+                    }
+                },
+                "aboutme": {
+                    "label": "__MSG__PROFILE_ABOUTME_LABEL__",
+                    "required": true,
+                    "display": true,
+                    "access": "everybody",
+                    "modifyacl": true,
+                    "permission": "everyone",
+                    "order": 2,
+                    "elements": {
+                        "aboutme": {
+                            "label": "__MSG__PROFILE_ABOUTME_LABEL__",
+                            "required": false,
+                            "display": true,
+                            "type": "textarea"
+                        },
+                        "academicinterests": {
+                            "label": "__MSG__PROFILE_ABOUTME_ACADEMICINTERESTS_LABEL__",
+                            "required": false,
+                            "display": true,
+                            "type": "textarea"
+                        },
+                        "personalinterests": {
+                            "label": "__MSG__PROFILE_ABOUTME_PERSONALINTERESTS_LABEL__",
+                            "required": false,
+                            "display": true,
+                            "type": "textarea"
+                        }
+                    }
+                },
+                "online": {
+                    "label": "__MSG__PROFILE_ONLINE_LABEL__",
+                    "required": false,
+                    "display": true,
+                    "access": "everybody",
+                    "modifyacl": true,
+                    "permission": "everyone",
+                    "multiple": true,
+                    "multipleLabel": "__MSG__PROFILE_ONLINE_LABEL__",
+                    "order": 3,
+                    "elements": {
+                        "siteOnline": {
+                            "label": "__MSG__PROFILE_ONLINE_SITE_LABEL__",
+                            "errorMessage": "__MSG__PROFILE_ONLINE_SITE_ERROR__",
+                            "required": true,
+                            "display": true,
+                            "example": "__MSG__PROFILE_ONLINE_SITE_EXAMPLE__"
+                        },
+                        "urlOnline": {
+                            "label": "__MSG__PROFILE_ONLINE_URL_LABEL__",
+                            "errorMessage": "__MSG__PROFILE_ONLINE_URL_ERROR__",
+                            "required": true,
+                            "display": true,
+                            "example": "__MSG__PROFILE_ONLINE_URL_EXAMPLE__"
+                        }
+                    }
+                },
+                "contact": {
+                    "label": "__MSG__PROFILE_CONTACT_LABEL__",
+                    "required": false,
+                    "display": true,
+                    "access": "everybody",
+                    "modifyacl": true,
+                    "permission": "everyone",
+                    "multiple": true,
+                    "order": 4,
+                    "elements": {
+                        "college": {
+                            "label": "__MSG__PROFILE_BASIC_COLLEGE_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "department": {
+                            "label": "__MSG__PROFILE_BASIC_DEPARTMENT_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "role": {
+                            "label": "__MSG__PROFILE_BASIC_ROLE_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "emailContact": {
+                            "label": "__MSG__PROFILE_CONTACT_EMAIL_LABEL__",
+                            "errorMessage": "__MSG__PROFILE_CONTACT_EMAIL_ERROR__",
+                            "required": false,
+                            "display": true,
+                            "type": "email"
+                        },
+                        "imContact": {
+                            "label": "__MSG__PROFILE_CONTACT_IM_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "phoneContact": {
+                            "label": "__MSG__PROFILE_CONTACT_PHONE_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "mobileContact": {
+                            "label": "__MSG__PROFILE_CONTACT_MOBILE_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "faxContact": {
+                            "label": "__MSG__PROFILE_CONTACT_FAX_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "addressContact": {
+                            "label": "__MSG__PROFILE_CONTACT_ADDRESS_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "cityContact": {
+                            "label": "__MSG__PROFILE_CONTACT_CITY_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "stateContact": {
+                            "label": "__MSG__PROFILE_CONTACT_STATE_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "postalContact": {
+                            "label": "__MSG__PROFILE_CONTACT_POSTAL_LABEL__",
+                            "required": false,
+                            "display": true
+                        },
+                        "countryContact": {
+                            "label": "__MSG__PROFILE_CONTACT_COUNTRY_LABEL__",
+                            "required": false,
+                            "display": true
+                        }
+                    }
+                },
+                "publications": {
+                    "label": "__MSG__PROFILE_PUBLICATIONS_LABEL__",
+                    "required": false,
+                    "display": true,
+                    "access": "everybody",
+                    "modifyacl": true,
+                    "permission": "everyone",
+                    "multiple": true,
+                    "multipleLabel": "__MSG__PROFILE_PUBLICATION_LABEL__",
+                    "order": 5,
+                    //"template": "profile_section_publications_template",
+                    "elements": {
+                        "maintitle": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_MAIN_TITLE__",
+                            "required": true,
+                            "display": true,
+                            "example": "__MSG__PROFILE_PUBLICATIONS_MAIN_TITLE_EXAMPLE__"
+                        },
+                        "mainauthor": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_MAIN_AUTHOR__",
+                            "required": true,
+                            "display": true
+                        },
+                        "coauthor": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_CO_AUTHOR__",
+                            "required": false,
+                            "display": true,
+                            "example": "__MSG__PROFILE_PUBLICATIONS_CO_AUTHOR_EXAMPLE__"
+                        },
+                        "publisher": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_PUBLISHER__",
+                            "required": true,
+                            "display": true
+                        },
+                        "placeofpublication": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_PLACE_OF_PUBLICATION__",
+                            "required": true,
+                            "display": true
+                        },
+                        "volumetitle": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_VOLUME_TITLE__",
+                            "required": false,
+                            "display": true
+                        },
+                        "volumeinformation": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_VOLUME_INFORMATION__",
+                            "required": false,
+                            "display": true,
+                            "example": "__MSG__PROFILE_PUBLICATIONS_VOLUME_INFORMATION_EXAMPLE__"
+                        },
+                        "year": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_YEAR__",
+                            "required": true,
+                            "display": true
+                        },
+                        "number": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_NUMBER__",
+                            "required": false,
+                            "display": true
+                        },
+                        "series title": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_SERIES_TITLE__",
+                            "required": false,
+                            "display": true
+                        },
+                        "url": {
+                            "label": "__MSG__PROFILE_PUBLICATIONS_URL__",
+                            "required": false,
+                            "display": true,
+                            "validation": "appendhttp url"
+                        }
+                    }
+                }
+            }
+        },
+        /*
+         * set what name to display where only the first name is used
+         */
+        userFirstNameDisplay: "firstName",
+
+        /*
+         * set how the user's name is displayed across the entire system
+         * - values can be compound, like "firstName lastName" or singular like "displayName"
+         */
+        userNameDisplay: "firstName lastName",
+
+        /*
+         * the default, if the user doesn't have the userNameDisplay property set in their
+         * profile, use this one.
+         * Note: the value for userNameDisplay and this value can be the same.
+         *       If neither exists, nothing will show
+         */
+        userNameDefaultDisplay: "firstName lastName",
+
+        /*
+         * Set the user's short description to appear underneath their name
+         * in search results
+         */
+        userShortDescription: "${role} in ${department} at ${college}",
+        groupShortDescription: "asdf"
+    };
+
+    /*
+     * Configure default user dashboard navigation and widgets.
+     */
     config.defaultprivstructure = {
         "structure0": {
             "dashboard": {
